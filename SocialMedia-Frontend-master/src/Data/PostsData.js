9dd27e5176a63f37d24911752480aad5a33c247b
Nandhi-tha <126787748+Nandhi-tha@users.noreply.github.com>
2023-05-14 17:04:28 +0530

    Add files via upload

---
+++ SocialMedia-Frontend-master/src/Data/PostsData.js
@@ -0,0 +1,29 @@
+import postPic1 from '../img/postpic1.jpg'
+import postPic2 from '../img/postpic2.jpg'
+import postPic3 from '../img/postpic3.JPG'
+
+
+export const PostsData = [
+    {
+        img: postPic1,
+        name: 'Jessie',
+        desc: "Happy New Year all friends! #2023",
+        likes: 50,
+        liked: true
+    },
+    {
+        img: postPic2,
+        name: 'Riley',
+        desc: "Party time :)",
+        likes: 65,
+        liked: false
+
+    },
+    {
+        img:postPic3,
+        name: "Marley",
+        desc: "At Archery Festival",
+        likes: 33,
+        liked: false
+    }
+]
